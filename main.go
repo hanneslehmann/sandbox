abc71e0b69fcbd76e8ff67f74ab9bb3b19c73738
Hannes Lehmann <hanneslehmann@users.noreply.github.com>
2019-03-26 20:58:11 +0100

    Update main.go

--- main.go
+++ main.go
@@ -4,13 +4,13 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
-	"syscall"
+	//"syscall"
 )
 
 func main() {
 	switch os.Args[1] {
 	case "run":
-		parent()
+		()
 	case "child":
 		child()
 	default:
@@ -18,11 +18,12 @@ func main() {
 	}
 }
 
-func parent() {
+func run() {
 	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
+	
 
 	if err := cmd.Run(); err != nil {
 		fmt.Println("ERROR", err)
